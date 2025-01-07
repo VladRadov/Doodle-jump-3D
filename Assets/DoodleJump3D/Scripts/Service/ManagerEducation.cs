83c1f2901f3293666f9119ec519671957439948a
VladRadov <picvladislav@yandex.ru>
2025-01-08 01:41:31 +0300

    feat: animation clue panels

--- Assets/DoodleJump3D/Scripts/Service/ManagerEducation.cs
+++ Assets/DoodleJump3D/Scripts/Service/ManagerEducation.cs
@@ -1,10 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
 
 public class ManagerEducation : BaseManager
 {
+    private const float START_Y_POSITION = 600;
+    private const float END_Y_POSITION = 350;
+    private const float SPEED_MOVE_Y_POSITION = 0.3f;
+
     [SerializeField] private GameObject _parentPanel;
     [SerializeField] private ClueView _clue1;
     [SerializeField] private ClueView _clue2;
@@ -15,26 +19,14 @@ public class ManagerEducation : BaseManager
 
     public override void Initialize()
     {
-        _clue1.OnOkClick.Subscribe(_ =>
+        _clue1.OnOkClick.Subscribe(_ => { MoveCluePanel(_clue1, _clue2); });
+        _clue2.OnOkClick.Subscribe(_ => { MoveCluePanel(_clue2, _clue3); });
+        _clue3.OnOkClick.Subscribe(_ => { MoveCluePanel(_clue3, _clue4); });
+        _clue4.OnOkClick.Subscribe(async _ =>
         {
-            _clue1.SetActive(false);
-            _clue2.SetActive(true);
-        });
+            _clue4.transform.DOMoveY(START_Y_POSITION, SPEED_MOVE_Y_POSITION);
+            await UniTask.Delay(500);
 
-        _clue2.OnOkClick.Subscribe(_ =>
-        {
-            _clue2.SetActive(false);
-            _clue3.SetActive(true);
-        });
-
-        _clue3.OnOkClick.Subscribe(_ =>
-        {
-            _clue3.SetActive(false);
-            _clue4.SetActive(true);
-        });
-
-        _clue4.OnOkClick.Subscribe(_ =>
-        {
             _clue4.SetActive(false);
             OnEducationEnd.Execute();
         });
@@ -43,7 +35,21 @@ public class ManagerEducation : BaseManager
     }
 
     public void SetActive(bool value)
-        => _parentPanel.gameObject.SetActive(value);
+    {
+        _parentPanel.gameObject.SetActive(value);
+        _clue1.transform.DOMoveY(END_Y_POSITION, SPEED_MOVE_Y_POSITION);
+    }
+
+    private async void MoveCluePanel(ClueView clueNoActive, ClueView clueActive)
+    {
+        clueActive.SetActive(true);
+
+        clueNoActive.transform.DOMoveY(START_Y_POSITION, SPEED_MOVE_Y_POSITION);
+        await UniTask.Delay(500);
+        clueActive.transform.DOMoveY(END_Y_POSITION, SPEED_MOVE_Y_POSITION);
+
+        clueNoActive.SetActive(false);
+    }
 
     private void OnDestroy()
     {
