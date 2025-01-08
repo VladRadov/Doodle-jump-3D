8d6ede9cfa6863311d8b78c814b0a6ad2483169d
VladRadov <picvladislav@yandex.ru>
2025-01-08 21:58:19 +0300

    fix: animation clue panels

--- Assets/DoodleJump3D/Scripts/Service/ManagerEducation.cs
+++ Assets/DoodleJump3D/Scripts/Service/ManagerEducation.cs
@@ -5,9 +5,9 @@ using Cysharp.Threading.Tasks;
 
 public class ManagerEducation : BaseManager
 {
-    private const float START_Y_POSITION = 600;
-    private const float END_Y_POSITION = 350;
-    private const float SPEED_MOVE_Y_POSITION = 0.3f;
+    private Vector2 START_Y_POSITION = new Vector2(502, -232);
+    private Vector2 END_Y_POSITION = new Vector2(502, 500);
+    private float SPEED_MOVE_Y_POSITION = 0.3f;
 
     [SerializeField] private GameObject _parentPanel;
     [SerializeField] private ClueView _clue1;
@@ -24,7 +24,7 @@ public class ManagerEducation : BaseManager
         _clue3.OnOkClick.Subscribe(_ => { MoveCluePanel(_clue3, _clue4); });
         _clue4.OnOkClick.Subscribe(async _ =>
         {
-            _clue4.transform.DOMoveY(START_Y_POSITION, SPEED_MOVE_Y_POSITION);
+            MoveCluePanel(null, _clue4);
             await UniTask.Delay(500);
 
             _clue4.SetActive(false);
@@ -37,18 +37,27 @@ public class ManagerEducation : BaseManager
     public void SetActive(bool value)
     {
         _parentPanel.gameObject.SetActive(value);
-        _clue1.transform.DOMoveY(END_Y_POSITION, SPEED_MOVE_Y_POSITION);
+        MoveCluePanel(null, _clue1);
     }
 
     private async void MoveCluePanel(ClueView clueNoActive, ClueView clueActive)
     {
-        clueActive.SetActive(true);
+        clueActive?.SetActive(true);
 
-        clueNoActive.transform.DOMoveY(START_Y_POSITION, SPEED_MOVE_Y_POSITION);
-        await UniTask.Delay(500);
-        clueActive.transform.DOMoveY(END_Y_POSITION, SPEED_MOVE_Y_POSITION);
+        if (clueNoActive != null)
+        {
+            var recTransformClueNoActive = clueNoActive.GetComponent<RectTransform>();
+            recTransformClueNoActive.DOAnchorPos(END_Y_POSITION, SPEED_MOVE_Y_POSITION);
+        }
+        if (clueActive != null)
+        {
+            await UniTask.Delay(500);
 
-        clueNoActive.SetActive(false);
+            var recTransformClueActive = clueActive.GetComponent<RectTransform>();
+            recTransformClueActive.DOAnchorPos(START_Y_POSITION, SPEED_MOVE_Y_POSITION);
+        }
+
+        clueNoActive?.SetActive(false);
     }
 
     private void OnDestroy()
