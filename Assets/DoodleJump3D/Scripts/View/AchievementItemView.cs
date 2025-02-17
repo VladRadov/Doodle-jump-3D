3e647ede6df86910ca8de5f02ac56b54060c288e
VladRadov <picvladislav@yandex.ru>
2025-02-18 00:25:40 +0300

    fix: fixed font

--- Assets/DoodleJump3D/Scripts/View/AchievementItemView.cs
+++ Assets/DoodleJump3D/Scripts/View/AchievementItemView.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+
 using TMPro;
 
 public class AchievementItemView : MonoBehaviour
@@ -8,11 +9,32 @@ public class AchievementItemView : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _name;
     [SerializeField] private TextMeshProUGUI _description;
     [SerializeField] private Image _icon;
+    [Header("Data")]
+    [SerializeField] private Achievement _achievement;
 
     public void Initialize(Achievement achievement)
     {
-        _name.text = achievement.Name;
-        _description.text = achievement.Description;
-        _icon.sprite = achievement.Icon;
+        _achievement = achievement;
+
+        SetAchivementDescription();
+        SetActiveAchivementItem();
     }
+
+    private void OnEnable()
+    {
+        SetAchivementDescription();
+    }
+
+    private void SetAchivementDescription()
+    {
+        if (_achievement != null)
+        {
+            _name.text = _achievement.Name;
+            _description.text = _achievement.Description;
+            _icon.sprite = _achievement.Icon;
+        }
+    }
+
+    private void SetActiveAchivementItem()
+        => _icon.color = _achievement.IsAchievementSuccess ? new Color(1, 1, 1, 1) : new Color(1, 1, 1, 0.5f);
 }
